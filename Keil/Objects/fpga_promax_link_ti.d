38c61586b95621834fb46e922056a406e6789e1b
hry <heyu51582022@163.com>
2026-07-23 21:00:22 +0800

    Fix .uvprojx paths: ../../Keil/Modules -> ../Modules

---
+++ Keil/Objects/fpga_promax_link_ti.d
@@ -0,0 +1,4 @@
+./objects/fpga_promax_link_ti.o: ..\Port\fpga_promax_link_ti.c \
+  ..\Modules\fpga_promax_link.h ..\Modules\fpga_promax.h \
+  ..\Modules\fpga_promax_link_map.h ..\Modules\fpga_link.h \
+  ..\Port\main.h ..\Port\stm32h7xx_hal.h
---
+++ Keil/Objects/nuedc_port.d
@@ -0,0 +1,7 @@
+./objects/nuedc_port.o: ..\Src\nuedc_port.c ..\Inc\nuedc_port.h \
+  ..\BSP\adc_app.h ..\Port\main.h ..\Port\stm32h7xx_hal.h \
+  ..\Modules\ad5687_app.h ..\Modules\ad5687.h ..\Modules\ad9910_app.h \
+  ..\Modules\ad9910.h ..\Modules\ad9708_app.h ..\Modules\ad9708.h \
+  ..\Modules\fpga_link.h ..\Modules\ad9280_app.h ..\Modules\ad9280.h \
+  ..\Modules\ads8363_app.h ..\Modules\ADS8363.h ..\Modules\dac8831_app.h \
+  ..\Modules\dac8831.h ..\Modules\screen_test.h
